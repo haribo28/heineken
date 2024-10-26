0007f7dcd645850562efb63c29ddfa10eb3b0576
haribo28 <soyoonk0625@naver.com>
2024-10-26 20:06:14 +0900

    업데이트

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ const storySlider = new Swiper(".story-slider", {
     },
 });
 
-const swiper = new Swiper(".products-slider", {
+const productsSlider = new Swiper(".products-slider", {
     // Optional parameters
     direction: "horizontal",
     loop: true,
@@ -26,4 +26,48 @@ const swiper = new Swiper(".products-slider", {
         nextEl: ".products-slider-wrap .products-next",
         prevEl: ".products-slider-wrap .products-prev",
     },
+});
+
+const encouragingSwiper = new Swiper(".encouraging-swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    effect: "cards",
+    grabCursor: true,
+
+    // // Navigation arrows
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+});
+
+$(document).ready(function () {
+    const $counters = $(".events-con");
+
+    const exposurePercentage = 80;
+    const loop = true;
+
+    $(window)
+        .on("scroll", function () {
+            $counters.each(function () {
+                const $el = $(this);
+
+                const rect = $el[0].getBoundingClientRect();
+                const winHeight = window.innerHeight;
+                const contentHeight = rect.bottom - rect.top;
+
+                if (
+                    rect.top <= winHeight - (contentHeight * exposurePercentage) / 100 &&
+                    rect.bottom >= (contentHeight * exposurePercentage) / 100
+                ) {
+                    $el.addClass("active");
+                }
+                if (loop && rect.top >= window.innerHeight) {
+                    $el.removeClass("active");
+                }
+            });
+        })
+        .scroll();
 });
